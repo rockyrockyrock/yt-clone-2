93b58098996860479aee402fdce0999b3e888a44
alex <alexlu851104@gmail.com>
2023-11-01 20:24:33 -0700

    first commit

--- yt-web-client/app/firebase/firebase.ts
+++ yt-web-client/app/firebase/firebase.ts
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {
-    getAuth,
-    signInWithPopup,
-    GoogleAuthProvider,
-    onAuthStateChanged,
-    User
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  User,
 } from "firebase/auth";
-import { getFunctions } from 'firebase/functions';
+import { getFunctions } from "firebase/functions";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDkmEOrJyoM40EKkZSILGUEZbfjZ9fn7V0",
+  apiKey: process.env.FIREBASE_CONFIG_API_KEY,
   authDomain: "yt-clone-al.firebaseapp.com",
   projectId: "yt-clone-al",
-  appId: "1:671293391527:web:19113bc7f023e713a55182"
+  appId: process.env.FIREBASE_CONFIG_APP_ID,
 };
 
 // Initialize Firebase
@@ -28,15 +28,15 @@ export const functions = getFunctions();
  * @returns A promise that resolves with the user's credentials.
  */
 export function signInWithGoogle() {
-    return signInWithPopup(auth, new GoogleAuthProvider());
+  return signInWithPopup(auth, new GoogleAuthProvider());
 }
 
 /**
  * Signs the user out.
  * @returns A promise that resolves when the user is signed out.
  */
-export function signOut(){
-    return auth.signOut();
+export function signOut() {
+  return auth.signOut();
 }
 
 /**
@@ -44,6 +44,8 @@ export function signOut(){
  * onAuthStateChanged is a listener for authentication state changes
  * @returns A function to unsubscribe callback.
  */
-export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
-    return onAuthStateChanged(auth, callback);
+export function onAuthStateChangedHelper(
+  callback: (user: User | null) => void
+) {
+  return onAuthStateChanged(auth, callback);
 }
